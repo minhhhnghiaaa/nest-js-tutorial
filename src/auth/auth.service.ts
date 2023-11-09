2bf602b2b081c96d1b7e7a340cd3f117cb506788
Tran Minh Nghia <ohmybaby1999@gmail.com>
2023-11-09 07:07:57 +0000

    update auth dto and prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  signin() {
+  constructor(private prisma: PrismaService) { }
+  signup() {
     return 'im gg';
   }
-  signup() {
+  signin() {
     return 'im gg';
   }
 }
